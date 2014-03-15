33f85e8bcff01b003b6c15d6ada3c733463da458
Charles Hooper <chooper@plumata.com>
2014-03-15 10:13:09 -0700

    return all headers

--- urinfo.py
+++ urinfo.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 # needed to perform remote HTTP HEAD request on URI
 import requests
 
-# create headers to pass when executing HTTP  requests
+# create headers to pass when executing HTTP requests
 USERAGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/534.34 (KHTML, like Gecko) Qt/4.8.3 Safari/534.34 urinfo"
 HEADERS = {'User-Agent' : USERAGENT}
 
@@ -26,11 +26,16 @@ def urinfo( uri ):
 
     info = {}
     info['uri'] = uri
+
+    # TODO: Exclude cookies and possibly some other headers
+    info['headers'] = dict(result.headers)
+
+    # the next 2 headers left for backwards compatibility
     info['content-type'] = result.headers.get('content-type', None)
     info['content-length'] = result.headers.get('content-length', None)
 
-    if info['content-type'] != None:
-        if 'html' in info['content-type']:
+    if info['headers'].get('content-type') != None:
+        if 'html' in info['headers']['content-type']:
             result = requests.get(uri, headers=HEADERS, allow_redirects=True, timeout=4.0)
             soup = BeautifulSoup(result.content)
             if soup.title:
