bddf35551d34f21649e442b5d7aa417b05443b29
Charles Hooper <chooper@plumata.com>
2014-09-21 13:55:04 -0700

    add rollbar exception tracking

--- app.py
+++ app.py
@@ -10,8 +10,9 @@ import os
 
 from flask import Flask
 from flask import jsonify
-from flask import render_template, request, redirect, url_for, abort
-
+from flask import render_template, request, redirect, url_for, abort, got_request_exception
+import rollbar
+import rollbar.contrib.flask
 from urinfo import urinfo
 
 app = Flask(__name__)
@@ -19,6 +20,18 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configur
 
 if os.environ.get('DEBUG', 0) > 1:
     app.debug = True
+
+rollbar_access_key = os.environ.get('ROLLBAR_ACCESS_KEY')
+if rollbar_access_key:
+    rollbar.init(
+        rollbar_access_key,
+        'urinfo',
+        root=os.path.dirname(os.path.realpath(__file__)),
+        allow_logging_basic_config=False
+    )
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 ###
 # Routing for your application.
